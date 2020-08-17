aa580bd16a2abdd57f818f21daf0e706a6bd5846
alexangria1992 <alex040892@gmail.com>
2020-08-18 11:11:34 +1200

    connected to database

---
+++ dbConnection.php
@@ -0,0 +1,21 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "newosms";
+    $db_port = 3306;
+
+    //Create Connection
+    $conn = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
+
+    //Checking Connection
+
+    if($conn->connect_error)
+    {
+        die("connection Failed");
+    }
+    else 
+    {
+        echo "Connect";
+    }
+?>
