e40df1283815cee0fc80c0f53bd7730dd63a5ea1
alexangria1992 <alex040892@gmail.com>
2020-08-17 12:18:31 +1200

    Created navigation bar

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-<?php echo "Hello"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OMS</title>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/custom.css">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-danger pl-5 fixed-top">
+        <a href="index.php" class="navbar-brand">OMS </a>
+        <span class="navbar-text">Customer's Happiness is our Aim</span>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myMenu">
+            <ul class="navbar-nav pl-5 custom-nav">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#Services" class="nav-link">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#Registration" class="nav-link">Registration</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#Login" class="nav-link">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
+
+
+</body>
+</html>
