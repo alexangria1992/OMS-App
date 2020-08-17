9af5adf5338af3618f8dc62f2afd32b95ef55de9
alexangria1992 <alex040892@gmail.com>
2020-08-17 12:53:08 +1200

    added paragraph and our services div

--- index.php
+++ index.php
@@ -50,6 +50,51 @@
     </header>
     
 
+    <div class="container">
+        <div class="jumbotron">
+            <h3 class="text-center">OSM</h3>
+            <p>
+            OSMS Services is India’s leading chain of multi-brand Electronics and Electrical service
+            workshops offering
+            wide array of services. We focus on enhancing your uses experience by offering world-class
+            Electronic
+            Appliances maintenance services. Our sole mission is “To provide Electronic Appliances care
+            services to
+            keep the devices fit and healthy and customers happy and smiling”.
+
+            With well-equipped Electronic Appliances service centres and fully trained mechanics, we
+            provide quality
+            services with excellent packages that are designed to offer you great savings.
+
+            Our state-of-art workshops are conveniently located in many cities across the country. Now you
+            can book
+            your service online by doing Registration.
+            </p>
+        </div>
+    </div>
+
+    <div class="container text-center border-bottom">
+        <h2>Our Services</h2>
+        <div class="row mt-4">
+            <div class="col-sm-4">
+                <a href="#"><i class="fas fa-tv fa-8x text-success"></i></a>
+                <h4 class="mt-4">Electronic Applicances</h4>
+            </div>
+            <div class="col-sm-4">
+                <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+                <h4 class="mt-4">Preventive Maintenance</h4>
+            </div>
+            <div class="col-sm-4">
+                <a href="#"><i class="fas fa-cogs fa-8x text-info"></i></a>
+                <h4 class="mt-4">Fault Repair</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="container pt-5">
+
+    </div>
+
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
